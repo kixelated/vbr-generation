38e5341e8877e1c31a227335a756d100eb52e33a
kixelated <kixelated@gmail.com>
2022-12-13 09:37:17 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,17 +2,39 @@
 set -euo pipefail
 
 # Default values that can be overriden when calling this script
+
+# TODO figure out the path on windows.
 : "${FFMPEG:=/c/Users/kixel/Videos/VBR/ffmpeg/bin/ffmpeg.exe}"
+
+# The input file. TODO use a relative path.
 : "${INPUT:=/c/Users/kixel/Videos/Demo/10games.avi}"
-: "${FPS:=60}"
+
+# TODO support vmaf when the resolution is not the same as the input file.
 : "${RESOLUTION:=1920x1080}"
+
+# The number of frames per second.
+: "${FPS:=60}"
+
+# The preset, defaulting to the nvenc P6
 : "${PRESET:=p6}"
+
+# The max bitrate in Mb/s
 : "${BITRATE:=6}"
+
+# The GoP duration in seconds
 : "${GOP:=2}"
 
+# Set to 1 to generate CBR as reference
+: "${CBR:=1}"
+
+# Set to the minimum and maximum CQ values
+: "${CQ_MIN:=24}"
+: "${CQ_MAX:=40}"
+
+# Generate a single rindition using the provided CQ
 generate() {
 	CQ="$1"
-	OUTPUT="${RESOLUTION}-${FPS}fps-${PRESET}-${BITRATE}M"
+	OUTPUT="${RESOLUTION}-${FPS}-${PRESET}-${BITRATE}M"
 	
 	# Disable stdout
 	FLAGS=" -hide_banner -loglevel error"
@@ -38,6 +60,11 @@ generate() {
 	# Use the provided FPS
 	FLAGS+=" -f:v fps=${FPS}"
 	
+	# Make sure we're using YUV
+	#FLAGS+=" -pix_fmt yuv420p"
+	
+	# Enable AQ, which is default in OBS for some reason
+	#FLAGS+=" -spatial-aq 1 -temporal-aq 1 -aq-strength 7"
 
 	if [ "$CQ" -eq "0" ]; then
 		# CBR mode
@@ -82,10 +109,10 @@ generate() {
 	FLAGS+=" -i ${INPUT}"
 	
 	# Use libvmaf to generate VMAF and PSNR scores
-	FLAGS+="-lavfi '[0][1]libvmaf=log_path=${OUTPUT}.json:log_fmt=json:psnr=1:n_threads=4'"
+	FLAGS+=" -lavfi [0][1]libvmaf=log_path=${OUTPUT}.json:log_fmt=json:psnr=1:n_threads=4"
 	
 	# Don't output any files
-	FLAGS+="-f null -"
+	FLAGS+=" -f null -"
 	
 	# Run FFMPEG with these flags to analyze the file
 	$FFMPEG $FLAGS
@@ -96,16 +123,19 @@ generate() {
 	RESULT_SIZE=$( stat -c%s "${OUTPUT}.mp4" )
 	
 	# Echo the results in CSV format
-	echo "${RESOLUTION},${FPS},${GOP},${PRESET},${BITRATE},${CQ},${RESULT_SIZE},${RESULT_PSNR},${RESULT_VMAF}"
+	echo "${RESOLUTION},${FPS},${GOP},${PRESET},${BITRATE}M,${CQ},${RESULT_SIZE},${RESULT_PSNR},${RESULT_VMAF}"
 }
 
 # Echo the header in CSV
 echo "resolution,fps,gop,preset,max bitrate,cq,size,psnr,vmaf"
 
-# CBR
-generate 0
+if [ "$CBR" -eq "1" ]; then
+	# CBR
+	generate 0
+fi
 
-for CQ in {30..40..1}; do
+# Loop over all of hte CQ values
+for CQ in $( seq $CQ_MIN $CQ_MAX ); do
 	# VBR
 	generate "${CQ}"
 done
